388d800af099e4a643bef9a5585a90b486f4e7a2
ritacvn <ritadecassia.vasconcelos@hotmail.com>
2025-03-04 22:09:25 -0400

    Creating file

---
+++ DisplayTV/DisplayTV/DisplayTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayTVApp.swift
+//  DisplayTV
+//
+//  Created by Rita Vasconcelos on 04/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DisplayTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisplayTV/DisplayTVUITests/DisplayTVUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  DisplayTVUITestsLaunchTests.swift
+//  DisplayTVUITests
+//
+//  Created by Rita Vasconcelos on 04/03/25.
+//
+
+import XCTest
+
+final class DisplayTVUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
